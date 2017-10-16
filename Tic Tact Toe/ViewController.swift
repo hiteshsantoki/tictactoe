227d2aae4df0911ced5ef555674efccec9d399fa
Cronabit 1 <cronabit1@Cronabits-Mac-mini.local>
2017-10-16 11:15:14 +0530

    Initial Commit

---
+++ Tic Tact Toe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tic Tact Toe
+//
+//  Created by Cronabit 1 on 16/10/17.
+//  Copyright © 2017 Cronabit 1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
